f537aebacb5b4df445b90770e7585a8645ef12c4
song <xudsong@163.com>
2020-12-13 15:22:32 +0800

    add 微信相关以及线程池的使用

--- src/main/java/com/xudasong/test/WeChatTestApplication.java
+++ src/main/java/com/xudasong/test/WeChatTestApplication.java
@@ -2,8 +2,9 @@ package com.xudasong.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class WeChatTestApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/xudasong/test/response/UnitListResp.java
@@ -0,0 +1,12 @@
+package com.xudasong.test.response;
+
+import lombok.Data;
+
+@Data
+public class UnitListResp {
+
+    private String code;
+
+    private String name;
+
+}
